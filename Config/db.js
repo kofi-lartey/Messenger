099fc413de648e289b81b0ba6cb9b8e42d45dcee
kofi-lartey <kofilartey12@gmail.com>
2026-01-31 17:52:53 +0000

    db fixed

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,5 @@ import { neon } from '@neondatabase/serverless'
 import { PGUSER, PGPASSWORD, PGDATABASE, PGHOST } from './env.js'
 
 export const sql = neon(
-    `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?
-    sslmode=require&
-    channel_binding=require`
+    `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`
 );
